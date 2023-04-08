d5482b9c5b500308aa956aed560c47d4e5cc0a0e
liu-qinglin666 <1343797887@qq.com>
2023-04-08 16:17:52 +0800

    fourth test finish

--- app/src/test/java/cn/edu/jnu/x2020104587/BowlingGameUnitTest.java
+++ app/src/test/java/cn/edu/jnu/x2020104587/BowlingGameUnitTest.java
@@ -38,7 +38,15 @@ public class BowlingGameUnitTest extends TestCase {
         assertEquals(16,game.score());
     }
     public void testOneStrike() throws Exception{
-
+        rollStrike();
+        game.roll(3);
+        game.roll(4);
+        rollMany(16,0);
+        assertEquals(24,game.score());
+    }
+    private void rollStrike()
+    {
+        game.roll(10);
     }
     private void rollSpace()
     {
